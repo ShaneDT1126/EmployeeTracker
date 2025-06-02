391f20e99fe15c52f3f46f61c0b495bf442e180b
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 15:05:01 +0800

    Add new enum status

---
+++ employeetrackerbackend/EmployeeTracker.Domain/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Enums
+{
+    public enum Status
+    {
+        [Display(Name = "Not Started")]
+        NotStarted = 0,
+        [Display(Name = "In Progress")]
+        InProgress = 1,
+        [Display(Name = "Completed")]
+        Completed = 2,
+    }
+}
