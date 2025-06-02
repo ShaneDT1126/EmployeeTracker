1c48042e1726ebbcd07c145e0a672d85db115722
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 14:49:26 +0800

    Add new entity evaluation criteria

---
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/EvaluationCriterias.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
+{
+    public class EvaluationCriterias
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
