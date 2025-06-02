f338e703e4c741530f5a4178d5155b383ec4568c
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 15:14:25 +0800

    Remove evaluation criteria entity,will make it an enum instead

--- employeetrackerbackend/EmployeeTracker.Domain/Entities/EvaluationCriterias.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
-{
-    public class EvaluationCriterias
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
