c2e4499420fb1c959ad6c53cc52a95c5064d1f3a
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 15:48:34 +0800

    Add new entity feedbacks

---
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/Feedbacks.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeTrackerBackend.EmployeeTracker.Domain.Enums;
+
+namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
+{
+    public class Feedbacks
+    {
+        public Guid Id { get; set; }
+        public Guid FromUserId { get; set; }
+        public Guid ToUserId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public EvaluationCriteriaType Tag { get; set; } 
+        public DateTime FeedbackDate { get; set; } = DateTime.UtcNow;
+    }
+}
