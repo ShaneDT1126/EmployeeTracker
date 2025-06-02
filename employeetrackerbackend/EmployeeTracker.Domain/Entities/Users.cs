58298aa0ba7e0fed543d889b1be2dea623250113
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 14:43:29 +0800

    Add new enum user role and modify user entity add new field role

--- employeetrackerbackend/EmployeeTracker.Domain/Entities/Users.cs
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/Users.cs
@@ -1,4 +1,6 @@
-﻿namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
+﻿using EmployeeTrackerBackend.EmployeeTracker.Domain.Enums;
+
+namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
 {
     public class Users
     {
@@ -7,6 +9,7 @@
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public UserRole role { get; set; } = UserRole.Employee; // Default role is Employee
         public string Role { get; set; } = string.Empty;
         public Guid? ManagerId { get; set; }
         public int? DepartmentId { get; set; }
---
+++ employeetrackerbackend/EmployeeTracker.Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Enums
+{
+    public enum UserRole
+    {
+        [Display(Name = "Employee")]
+        Employee = 1,
+        [Display(Name = "Manager")]
+        Manager = 2,
+        [Display(Name = "Admin")]
+        Admin = 3
+    }
+}
