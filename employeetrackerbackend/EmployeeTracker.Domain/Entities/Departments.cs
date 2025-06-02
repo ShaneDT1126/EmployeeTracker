d905dcc0f966245f1ff8c1efc0747a450f4581ae
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 14:46:31 +0800

    Add new entity Department

---
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/Departments.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
+{
+    public class Departments
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
