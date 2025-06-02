67e93d8e014c46f30033581bae64fec17e740089
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 14:32:59 +0800

    Modify folder structure and Add new entity User

---
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public Guid? ManagerId { get; set; }
+        public int? DepartmentId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
