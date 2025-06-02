3b982ef486924042251443587cfca4976463e8c6
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 15:47:49 +0800

    Modify evaluation criteria type with display names

--- employeetrackerbackend/EmployeeTracker.Domain/Enums/EvaluationCriteriaType.cs
+++ employeetrackerbackend/EmployeeTracker.Domain/Enums/EvaluationCriteriaType.cs
@@ -1,12 +1,20 @@
-﻿namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Enums
 {
     public enum EvaluationCriteriaType
     {
+        [Display(Name = "Communication Skills")]
         Communication = 0,
+        [Display(Name = "Teamwork")]
         Teamwork = 1,
+        [Display(Name = "Problem Solving")]
         ProblemSolving = 2,
+        [Display(Name = "Initiative")]
         Initiative = 3,
+        [Display(Name = "Attendance")]
         Attendance = 4,
+        [Display(Name = "Technical Skills")]
         TechnicalSkills = 5,
     }
 }
