f72fab118d61894170a951f7144640ae02573985
shijuchi <shanedelatorre2412@gmail.com>
2025-06-02 15:06:06 +0800

    Add new entity goals

---
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/Goals.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeTrackerBackend.EmployeeTracker.Domain.Enums;
+
+namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
+{
+    public class Goals
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Guid EmployeeId { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+        public DateTime EndDate { get; set; } = DateTime.UtcNow.AddMonths(1); // Default to one month from start date
+        public Status Status { get; set; } = Status.NotStarted; // Default status
+    }
+}
