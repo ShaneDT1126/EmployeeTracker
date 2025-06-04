2420f4336a51f3a6b49a774e7075c46860e78638
shijuchi <shanedelatorre2412@gmail.com>
2025-06-04 15:24:56 +0800

    Modify field comment

--- employeetrackerbackend/EmployeeTracker.Domain/Entities/EvaluationDetails.cs
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/EvaluationDetails.cs
@@ -9,7 +9,7 @@ namespace EmployeeTrackerBackend.EmployeeTracker.Domain.Entities
         public EvaluationCriteriaType EvaluationCriteriaType { get; set; }
         [Range(1, 5, ErrorMessage = "Score must be between 1 and 5.")]
         public int Score { get; set; }
-        public string? Comment { get; set; }
+        public string? Comment { get; set; } = string.Empty;
 
         // Freign key
         public Guid EvaluationId { get; set; }
