8771f7e1c4f1e3228bd0963578502903dce7c7a2
shijuchi <shanedelatorre2412@gmail.com>
2025-06-04 14:23:40 +0800

    Modify departments entity add the navigation properties

--- employeetrackerbackend/EmployeeTracker.Domain/Entities/Departments.cs
+++ employeetrackerbackend/EmployeeTracker.Domain/Entities/Departments.cs
@@ -4,5 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
+
+        // Navigation property for employees in this department
+        public ICollection<Users>? Employees { get; set; } = new List<Users>();
     }
 }
