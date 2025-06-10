17733d8ed781bdf0f0c234f6dc6fbe61c6ff8caf
shijuchi <shanedelatorre2412@gmail.com>
2025-06-10 13:32:52 +0800

    Modify file program.cs add dbcontext builders

--- employeetrackerbackend/EmployeeTracker.API/Program.cs
+++ employeetrackerbackend/EmployeeTracker.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Entity Framework Core with SQL Server support
+builder.Services.AddDbContext<EmployeeTrackerBackend.EmployeeTracker.Infrastructure.Data.ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
