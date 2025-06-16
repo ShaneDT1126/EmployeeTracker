76cdedccbd0b30d62023d209dcc91514cae5e320
shijuchi <shanedelatorre2412@gmail.com>
2025-06-16 10:56:19 +0800

    Add DTO tempalte files

--- EmployeeManagementSystem.Application/DTOs/Authentication/AuthDTO.cs
+++ EmployeeManagementSystem.Application/DTOs/Authentication/AuthDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeManagementSystem.Application.DTOs
+namespace EmployeeManagementSystem.Application.DTOs.Authentication
 {
     public class AuthDTO
     {
