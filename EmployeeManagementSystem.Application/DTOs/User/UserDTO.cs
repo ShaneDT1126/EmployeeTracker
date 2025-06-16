00462ae7419e7743d1cc1baf73104bb6e11b5ed2
shijuchi <shanedelatorre2412@gmail.com>
2025-06-16 16:45:52 +0800

    Modify UserDTO template now with fields

--- EmployeeManagementSystem.Application/DTOs/User/UserDTO.cs
+++ EmployeeManagementSystem.Application/DTOs/User/UserDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagementSystem.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace EmployeeManagementSystem.Application.DTOs.User
 {
     public class UserDTO
     {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.Employee; // Default role is Employee
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //Foreign keys
+        public Guid? ManagerId { get; set; }
+        public Guid? DepartmentId { get; set; }
     }
 }
