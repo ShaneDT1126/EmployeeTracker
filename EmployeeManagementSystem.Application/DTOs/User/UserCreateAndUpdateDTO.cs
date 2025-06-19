02d0487ecc545d2e09f001fc80cfd6360ae3f284
shijuchi <shanedelatorre2412@gmail.com>
2025-06-19 16:03:02 +0800

    Update create/update dto with data validations

--- EmployeeManagementSystem.Application/DTOs/User/UserCreateAndUpdateDTO.cs
+++ EmployeeManagementSystem.Application/DTOs/User/UserCreateAndUpdateDTO.cs
@@ -1,24 +1,35 @@
 ﻿using EmployeeManagementSystem.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeManagementSystem.Application.DTOs.User
 {
-    public class UserDTO
+    public class UserCreateAndUpdateDTO
     {
+        [Required(ErrorMessage = "User ID is required")]
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Role is required")]
         public UserRole Role { get; set; } = UserRole.Employee; // Default role is Employee
+        [Required(ErrorMessage = "Phone number is required")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         //Foreign keys
+        [Required(ErrorMessage = "Manager ID is required")]
         public Guid? ManagerId { get; set; }
+        [Required(ErrorMessage = "Department ID is required")]
         public Guid? DepartmentId { get; set; }
     }
 }
