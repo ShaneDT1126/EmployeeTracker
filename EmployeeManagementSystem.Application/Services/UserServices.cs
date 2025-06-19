8ba78dbf6102c1c4efd05c23ecd2f13540077b63
shijuchi <shanedelatorre2412@gmail.com>
2025-06-19 13:10:12 +0800

    Add User Serice Template

---
+++ EmployeeManagementSystem.Application/Services/UserServices.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagementSystem.Application.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Application.Services
+{
+    public class UserServices : Interfaces.Services.IUserService
+    {
+        public Task<UserDTO> CreateUserAsync(UserDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO?> DeleteUserAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDTO>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO?> GetUserByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO?> UpdateUserAsync(Guid id, UserDTO user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
