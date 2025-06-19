7ca4d9c192c81fe9cfc4a2828c3398a60a48d45a
shijuchi <shanedelatorre2412@gmail.com>
2025-06-19 16:37:05 +0800

    Update user controller add constructor with injected user service

--- EmployeeManagementSystem.API/Controllers/User/UserController.cs
+++ EmployeeManagementSystem.API/Controllers/User/UserController.cs
@@ -1,6 +1,18 @@
-﻿namespace EmployeeManagementSystem.API.Controllers.User
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.API.Controllers.User
 {
-    public class UserController
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly EmployeeManagementSystem.Application.Interfaces.Services.IUserService _userService;
+
+        public UserController(EmployeeManagementSystem.Application.Interfaces.Services.IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
     }
 }
