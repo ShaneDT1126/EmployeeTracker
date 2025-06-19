9bf042e626c240a1109d87536dc0390a695c2fea
shijuchi <shanedelatorre2412@gmail.com>
2025-06-19 15:08:47 +0800

    Update the user service using refractored dtos, mappers, and interface

--- EmployeeManagementSystem.Application/Services/UserServices.cs
+++ EmployeeManagementSystem.Application/Services/UserServices.cs
@@ -17,34 +17,34 @@ namespace EmployeeManagementSystem.Application.Services
             _userRepository = userRepository;
         }
 
-        public async Task<UserDTO> CreateUserAsync(UserDTO user, Guid requesterId, UserRole requestingRole)
+        public async Task<UserCreateAndUpdateDTO> CreateUserAsync(UserCreateAndUpdateDTO user, Guid requesterId, UserRole requestingRole)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<UserDTO?> DeleteUserAsync(Guid id, Guid requesterId, UserRole requestingRole)
+        public async Task<UserViewDTO?> DeleteUserAsync(Guid id, Guid requesterId, UserRole requestingRole)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<UserDTO>> GetAllUsersAsync(Guid requesterId, UserRole requestingRole)
+        public async Task<IEnumerable<UserViewDTO>> GetAllUsersAsync(Guid requesterId, UserRole requestingRole)
         {
             var users = await _userRepository.GetAllUsersAsync();
             if (!users.Any())
             {
-                return new List<UserDTO>();
+                return new List<UserViewDTO>();
             }
 
             return requestingRole switch
             {
-                UserRole.Admin => users.Select(u => u.ToAdminUserDTO()),
-                UserRole.Manager => users.Where(u => u.Id == requesterId).Select(u => u.ToManagerUserDTO()),
+                UserRole.Admin => users.Select(u => u.ToAdminUserViewDTO()),
+                UserRole.Manager => users.Where(u => u.ManagerId == requesterId).Select(u => u.ToManagerUserViewDTO()),
                 UserRole.Employee => throw new UnauthorizedAccessException("Employees cannot view all users."),
                 _ => throw new NotSupportedException("Unsupported user role.")
             };
         }
 
-        public async Task<UserDTO?> GetUserByIdAsync(Guid id, Guid requesterID, UserRole requestingRole)
+        public async Task<UserViewDTO?> GetUserByIdAsync(Guid id, Guid requesterID, UserRole requestingRole)
         {
             var user = await _userRepository.GetUserByIdAsync(id);
             if (user == null)
@@ -54,14 +54,15 @@ namespace EmployeeManagementSystem.Application.Services
 
             return requestingRole switch
             {
-                UserRole.Admin => user.ToAdminUserDTO(),
-                UserRole.Manager when user.Id == requesterID => user.ToManagerUserDTO(),
-                UserRole.Employee when user.Id == requesterID => user.ToEmployeeUserDTO(),
+                UserRole.Admin => user.ToAdminUserViewDTO(),
+                UserRole.Manager when user.Id == requesterID => user.ToManagerUserViewDTO(),
+                UserRole.Manager when user.ManagerId == requesterID => user.ToManagerUserViewDTO(),
+                UserRole.Employee when user.Id == requesterID => user.ToEmployeeUserViewDTO(),
                 _ => throw new UnauthorizedAccessException("You do not have permission to view this user.")
             };
         }
 
-        public async Task<UserDTO?> UpdateUserAsync(Guid id, UserDTO user, Guid requesterId, UserRole requestingRole)
+        public async Task<UserCreateAndUpdateDTO?> UpdateUserAsync(Guid id, UserCreateAndUpdateDTO user, Guid requesterId, UserRole requestingRole)
         {
             throw new NotImplementedException();
         }
