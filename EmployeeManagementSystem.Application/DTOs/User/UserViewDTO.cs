db9a9cf5925f80cf6064ab80b93d037a20e0b3f3
shijuchi <shanedelatorre2412@gmail.com>
2025-06-19 14:59:40 +0800

    Refractor DTOs for better code quality

---
+++ EmployeeManagementSystem.Application/DTOs/User/UserViewDTO.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeManagementSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Application.DTOs.User
+{
+    public class UserViewDTO
+    {
+        public Guid Id{ get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? ManagerName { get; set; } = string.Empty;
+        public string? DepartmentName { get; set; } = string.Empty;
+    }
+}
